3346a9ceb493a39074df5c5e7310e276642afabe
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2026-07-04 22:35:56 +0800

    feat: add @react-native-menu/menu for context menu actions in CapsuleItem component

--- src/app/capsule/index.tsx
+++ src/app/capsule/index.tsx
@@ -29,6 +29,5 @@ export default function Capsule() {
   );
 }
 
-// TODO VALIDATIONS
 //TODO Delete
 // TODO MODALS (for confirmations)
--- src/components/features/capsuleItem.tsx
+++ src/components/features/capsuleItem.tsx
@@ -1,6 +1,7 @@
 import { useSelectedCapsuleStore } from "@/store/useSelectedCapsuleStore";
 import { VideoMetadataType } from "@/types/types";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { MenuView } from "@react-native-menu/menu";
 import { router } from "expo-router";
 import { Pressable, View } from "react-native";
 import { AppText } from "../ui/appText";
@@ -8,35 +9,52 @@ import { AppText } from "../ui/appText";
 export function CapsuleItem({ metadata }: { metadata: VideoMetadataType }) {
   const isUnlocked = new Date(metadata.unlockDate) <= new Date();
   const setMetadata = useSelectedCapsuleStore((state) => state.setMetadata);
+
   const handleRedirect = () => {
     if (!isUnlocked) return;
     setMetadata(metadata);
     router.navigate("/capsule");
   };
+
   return (
-    <Pressable
-      className="flex w-full flex-row gap-5 rounded-2xl bg-surface-raised p-4 shadow-sm"
-      onPress={handleRedirect}
-    >
-      <View className="w-[20vw] items-center justify-center rounded-lg bg-text-primary">
-        <Ionicons
-          name={isUnlocked ? "play-outline" : "lock-closed-outline"}
-          size={30}
-          color="black"
-        />
-      </View>
-      <View className="mb-3 flex-1">
-        <AppText className="text-2xl font-semibold">{metadata.title}</AppText>
-        <AppText className="mt-1 text-text-muted">
-          Unlocks:
-          <AppText>
-            {isUnlocked ? "capsule is unlocked!" : metadata.unlockDate}
+    <View className="relative flex w-full flex-row gap-5 rounded-2xl bg-surface-raised p-4 shadow-sm">
+      <Pressable
+        className="flex flex-1 flex-row gap-5"
+        onPress={handleRedirect}
+      >
+        <View className="w-[20vw] items-center justify-center rounded-lg bg-text-primary">
+          <Ionicons
+            name={isUnlocked ? "play-outline" : "lock-closed-outline"}
+            size={30}
+            color="black"
+          />
+        </View>
+        <View className="mb-3 flex-1">
+          <AppText className="text-2xl font-semibold">{metadata.title}</AppText>
+          <AppText className="mt-1 text-text-muted">
+            Unlocks:
+            <AppText>
+              {isUnlocked ? "capsule is unlocked!" : metadata.unlockDate}
+            </AppText>
           </AppText>
-        </AppText>
-        <AppText className="mt-1 text-xs text-text-muted">
-          Created {metadata.createdAt}
-        </AppText>
-      </View>
-    </Pressable>
+          <AppText className="mt-1 text-xs text-text-muted">
+            Created {metadata.createdAt}
+          </AppText>
+        </View>
+      </Pressable>
+      <MenuView
+        onPressAction={({ nativeEvent }) => {
+          if (nativeEvent.event === "delete") {
+            //TODO DELETE POP UP ITEM
+          }
+        }}
+        actions={[
+          { id: "delete", title: "Delete", attributes: { destructive: true } },
+        ]}
+        style={{ position: "absolute", right: 12, top: 12, padding: 8 }}
+      >
+        <Ionicons name="ellipsis-vertical-outline" color="white" size={18} />
+      </MenuView>
+    </View>
   );
 }
